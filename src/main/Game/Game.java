84da5cae063e587ec0a0481fa0187aea25e60f94
loannjui <loann.juillerat@heig-vd.ch>
2025-05-20 11:31:37 +0200

    Changes

--- src/main/Game/Game.java
+++ src/main/Game/Game.java
@@ -1,19 +1,30 @@
 package main.Game;
 
+import main.Commands.CommandsRegistry;
 
 public class Game {
+    private static Game instance;
+    private static Map map;
+    private static Player player;
+    private static CommandsRegistry commandsRegistry;
 
-    public Game(){
+    private Game(Map map, Player player, CommandsRegistry commandsRegistry) {
         System.out.println("Initializing game...");
+        Game.map = map;
+        Game.player = player;
     }
-    
-    public void run() {
+
+    public static void run() {
         System.out.println("Running game...");
         // your runtime code here...
-
-
 
         // end of game
     }
 
+    public static Game getInstance() {
+        if (Game.instance == null) {
+            Game.instance = new Game(map, player, commandsRegistry);
+        }
+        return Game.instance;
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -11,14 +11,16 @@ import utils.Style;
 public class Main {
 
     public static void main(String[] args) {
-        // you can safely remove those examples lines
-        StringManipulation.Example();
-        ConsoleStylingExample.Example();
-        UserInputExample.Example();
-
-        System.out.println(StringStyling.StyleString("Starting...", Style.ITALIC, Color.BLACK));
-        Game game = new Game();
-        game.run();
-        System.out.println(StringStyling.StyleString("Terminating...", Style.ITALIC, Color.BLACK));
+        /*
+         * StringManipulation.Example();
+         * ConsoleStylingExample.Example();
+         * UserInputExample.Example();
+         */
+        // System.out.println(StringStyling.StyleString("Starting...", Style.ITALIC,
+        // Color.BLACK));
+        // System.out.println(StringStyling.StyleString("Terminating...", Style.ITALIC,
+        // Color.BLACK));
+        Game.getInstance();
+        Game.run();
     }
 }
