31e98c402c99a6fb23f990110ecddb1d96e15e9b
loannjui <loann.juillerat@heig-vd.ch>
2025-05-20 13:00:48 +0200

    Update Game.java

--- src/main/Game/Game.java
+++ src/main/Game/Game.java
@@ -2,42 +2,49 @@ package main.Game;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import main.Commands.CommandsRegistry;
+import main.Commands.ICommand;
 
 public class Game {
     private static Game instance;
-    private static Map map;
-    private static Player player;
-    private static CommandsRegistry commandsRegistry;
+
+    private Map map;
+    private Player player;
+    private CommandsRegistry commandsRegistry;
 
     private Game(Map map, Player player, CommandsRegistry commandsRegistry) {
         System.out.println("Initializing game...");
-        Game.map = map;
-        Game.player = player;
+        this.map = map;
+        this.player = player;
+        this.commandsRegistry = commandsRegistry;
     }
 
     public static void run() {
         System.out.println("Running game...");
-        // your runtime code here...
-
-        // end of game
     }
 
     public static Game getInstance() {
-        if (Game.instance == null) {
-          Player player = new Player("Player1", Arrays.asList(2, 2));
-          Map map = new Map(new ArrayList<>());
-            Game.instance = new Game(map, player, commandsRegistry);
-                    }
-        return Game.instance;
+        if (instance == null) {
+            Player player = new Player("Player1", Arrays.asList(2, 2));
+            Map map = new Map(new ArrayList<>());
+            java.util.Map<String, ICommand> commands = new HashMap<>();
+            CommandsRegistry registry = new CommandsRegistry(commands);
+            instance = new Game(map, player, registry);
+        }
+        return instance;
     }
 
-    public static Map getMap() {
+    public Map getMap() {
         return map;
     }
 
-    public static Player getPlayer() {
+    public Player getPlayer() {
         return player;
+    }
+
+    public CommandsRegistry getCommandsRegistry() {
+        return commandsRegistry;
     }
 }
