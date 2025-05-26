0764d77669e9ec830455a6e59b351ccb30ef95c0
loannjui <loann.juillerat@heig-vd.ch>
2025-05-26 09:07:43 +0200

    Update DisplayMap.java

--- src/main/Commands/DisplayMap.java
+++ src/main/Commands/DisplayMap.java
@@ -20,7 +20,8 @@ public class DisplayMap extends Command {
     public void execute(String argument) {
         List<List<IPrintable>> toPrint = toPrintableGrid(map.getLocationGrid());
         IPrintable[][] array2D = Array2Dprinter.convert2DArray(toPrint);
-        System.out.println(Array2Dprinter.print2DArray(array2D, player.getPlayerPosition().get(0), player.getPlayerPosition().get(1)));
+        String inputString = Array2Dprinter.print2DArray(array2D, player.getPlayerPosition().get(0), player.getPlayerPosition().get(1));
+        System.out.println(inputString);
     }
 
     public static List<List<IPrintable>> toPrintableGrid(List<List<Location>> grid) {
