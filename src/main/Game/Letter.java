b0417ff5effc2a77f264d1f0b43e849cf83770bd
TeTri12 <tess.triponez@hotmail.ch>
2025-05-30 19:51:01 +0200

    Adaptation de letter pour trimer les réponse au riddle

--- src/main/Game/Letter.java
+++ src/main/Game/Letter.java
@@ -2,19 +2,22 @@ package main.Game;
 
 public class Letter extends Item {
     private String answer;
-    private String locationNameToUnlock; /*Tess */
-    private boolean solved = false; /*Tess*/
+    private String locationNameToUnlock; /* Tess */
+    private boolean solved = false; /* Tess */
 
-    public Letter(String name, String description, String answer, String locationNameToUnlock /*Tess */) {
+    public Letter(String name, String description, String answer, String locationNameToUnlock /* Tess */) {
         super(name, description);
         this.answer = answer;
-        this.locationNameToUnlock = locationNameToUnlock; /*Tess */
+        this.locationNameToUnlock = locationNameToUnlock; /* Tess */
     }
 
-        public boolean checkAnswer(String input) { /*Tess */
-        return input.equalsIgnoreCase(answer);
+    public boolean checkAnswer(String input) {
+        return input.trim().equalsIgnoreCase(answer.trim());
     }
-        public boolean isSolved() {
+
+    }
+
+    public boolean isSolved() {
         return solved;
     }
 
@@ -22,13 +25,13 @@ public class Letter extends Item {
         solved = true;
     }
 
-    public String getLocationNameToUnlock() { /*Tess */
+    public String getLocationNameToUnlock() { /* Tess */
         return locationNameToUnlock;
     }
 
     @Override
     public void interactionsWithItem() {
         // TODO Auto-generated method stub
-        
+
     }
 }
