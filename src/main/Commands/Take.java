3ab6b83a082b8cb667a7622c41cca6f6619ea77c
loannjui <loann.juillerat@heig-vd.ch>
2025-06-02 08:34:31 +0200

    Update Take.java

--- src/main/Commands/Take.java
+++ src/main/Commands/Take.java
@@ -32,7 +32,6 @@ public void execute(String argument) {
                 inventory.addItem(i);
                 iterator.remove();
                 itemTaken = i.getName();
-                break;
             }
         }
 
